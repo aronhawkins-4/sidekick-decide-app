ee2b355d3bf2e3553150ca3d5cd7599854f18c7c
Aron Hawkins <aron@sidekick.agency>
2023-08-20 21:30:38 -0500

    remove unused files

--- src/app/api/restaurants/route.ts
+++ src/app/api/restaurants/route.ts
@@ -1,26 +1,26 @@
-// import prisma from '@/app/libs/prismadb';
-// import { NextResponse } from 'next/server';
-// import { pusherServer } from '@/app/libs/pusher';
+import prisma from '@/app/libs/prismadb';
+import { NextResponse } from 'next/server';
+import { pusherServer } from '@/app/libs/pusher';
 
-// export async function POST(request: Request) {
-// 	try {
-// 		const body = await request.json();
-// 		const { name, createdById, createdByName } = body;
-// 		if (!name || !createdById || !createdByName) {
-// 			return new NextResponse('Unauthorized', { status: 401 });
-// 		}
+export async function POST(request: Request) {
+	try {
+		const body = await request.json();
+		const { name, createdById, createdByName } = body;
+		if (!name || !createdById || !createdByName) {
+			return new NextResponse('Unauthorized', { status: 401 });
+		}
 
-// 		const newRestaurant = await prisma.restaurant.create({
-// 			data: {
-// 				name,
-// 				createdById,
-// 				createdByName,
-// 			},
-// 		});
+		const newRestaurant = await prisma.restaurant.create({
+			data: {
+				name,
+				createdById,
+				createdByName,
+			},
+		});
 
-// 		await pusherServer.trigger('restaurants-list', 'restaurant:new', newRestaurant);
-// 		return NextResponse.json(newRestaurant);
-// 	} catch (error: any) {
-// 		return new NextResponse('Internal error', { status: 500 });
-// 	}
-// }
+		await pusherServer.trigger('restaurants-list', 'restaurant:new', newRestaurant);
+		return NextResponse.json(newRestaurant);
+	} catch (error: any) {
+		return new NextResponse('Internal error', { status: 500 });
+	}
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,16 @@
 import { currentUser } from '@clerk/nextjs';
-import { RestaurantForm } from './components/RestaurantForm';
 import { RestaurantList } from './components/RestaurantList';
-import getRestaurants from './actions/getRestaurants';
-import getVotes from './actions/getVotes';
-import { UserCount } from './components/UserCount';
-import { RestaurantCard } from './components/RestaurantCard';
-import { LocationForm } from './components/LocationForm';
-import { IParams } from './actions/getGoogleRestaurants';
 
-import getGoogleRestaurants from './actions/getGoogleRestaurants';
-import { Suspense } from 'react';
-
-export default async function Home({ searchParams }: { searchParams: IParams }) {
+export default async function Home() {
 	const user = await currentUser();
-	// const restaurants = await getRestaurants();
-
-	// const gRes = await getGoogleRestaurants(searchParams);
 	if (!user) {
 		return <div>Please Login</div>;
 	}
 	return (
 		<main className='flex min-h-screen flex-col p-24'>
 			<div className='w-full h-full flex items-center justify-center flex-grow mt-6'>
-				{/* <RestaurantForm
-					userId={user.id}
-					userName={`${user.firstName} ${user.lastName}`}
-				/> */}
 				<RestaurantList userId={user.id} />
 			</div>
-			{/* <UserCount restaurantList={restaurants} /> */}
-			{/* <ul>
-				{gRes.map((res) => (
-					<li key={res.place_id}>{res.name}</li>
-				))}
-			</ul> */}
 		</main>
 	);
 }
